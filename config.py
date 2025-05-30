75c659654adb62defd90dc9eff83923b34fe7476
ignore528 <aliakram7548@gmail.com>
2025-05-30 19:38:53 +0300

    Update config.py

--- config.py
+++ config.py
@@ -45,8 +45,8 @@ GIT_TOKEN = getenv(
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
 API_KEY = getenv("API_KEY", '30DxNexGenBots299cf6') # youtube song api ke
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TEAM_CHOR")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+v40WYcSPzpU2Y2Y1")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+opI_UU0c4nxmODBl")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+-96WqPJaW6wyZGZl")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
